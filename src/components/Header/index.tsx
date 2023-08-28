54169fdc7baa7ffe60c022c57f7924c166b4cf02
Bruno Silva <bruno_silva2101@hotmail.com>
2023-08-28 19:51:34 -0300

    style: size icons in menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -25,9 +25,9 @@ export function Header() {
     <>
       <Content mode={mode}>
         <IconsConfig>
-          <Image className="__icon_menu" src={`/icons/sun-${mode}-mode.svg`} alt="icone de sol" width={32} height={32} onClick={handleChangeMode}/>
+          <Image className="__icon_menu" src={`/icons/sun-${mode}-mode.svg`} alt="icone de sol" width={26} height={26} onClick={handleChangeMode}/>
           <DropdownLanguage mode={mode}>
-            <Image className="__icon_menu" src={`/icons/languages-${mode}-mode.svg`} alt="icone de linguagem" width={32} height={32} onClick={() => setOpenDropdow(!openDropdow)}/>
+            <Image className="__icon_menu" src={`/icons/languages-${mode}-mode.svg`} alt="icone de linguagem" width={26} height={26} onClick={() => setOpenDropdow(!openDropdow)}/>
             {openDropdow && (
               <div className="__options_language">
                 {[...locales!].sort().map((locale) => (
@@ -41,16 +41,16 @@ export function Header() {
         </IconsConfig>
         <IconsNetwork>
           <Link href="https://www.linkedin.com/in/brunnosillvar" target="_blank">
-            <Image className="__icon_menu" src={`/icons/linkedin-${mode}-mode.svg`} alt="icone linkedin" width={32} height={32}/>
+            <Image className="__icon_menu" src={`/icons/linkedin-${mode}-mode.svg`} alt="icone linkedin" width={26} height={26}/>
           </Link>
           <Link href="https://github.com/brunnosillvar" target="_blank">
-            <Image className="__icon_menu" src={`/icons/github-${mode}-mode.svg`} alt="icone github" width={32} height={32}/>
+            <Image className="__icon_menu" src={`/icons/github-${mode}-mode.svg`} alt="icone github" width={26} height={26}/>
           </Link>
           <Link href="https://www.instagram.com/brunnosillvar" target="_blank">
-            <Image className="__icon_menu" src={`/icons/instagram-${mode}-mode.svg`} alt="icone instagram" width={32} height={32}/>
+            <Image className="__icon_menu" src={`/icons/instagram-${mode}-mode.svg`} alt="icone instagram" width={26} height={26}/>
           </Link>
           <Link href="https://www.behance.net/brunosilva130" target="_blank">
-            <Image className="__icon_menu" src={`/icons/dribbble-${mode}-mode.svg`} alt="icone dribbble" width={32} height={32}/>
+            <Image className="__icon_menu" src={`/icons/dribbble-${mode}-mode.svg`} alt="icone dribbble" width={26} height={26}/>
           </Link>
         </IconsNetwork>
       </Content>
