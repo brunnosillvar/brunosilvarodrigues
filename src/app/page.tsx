2211635f9b6f3f3d1316b0540fbcc6508c2d1df5
Bruno Silva <bruno_silva2101@hotmail.com>
2023-09-11 18:46:09 -0300

    fix: improvement redirect to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+// import { useRouter } from 'next/navigation'
+import { redirectToHome } from '@/helpers/redirectToHome'
 
 export default function Welcome() {
-  const router = useRouter()
-  router.push('/pt')
-  return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
-  )
+  redirectToHome()
 }
