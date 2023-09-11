9b1e892505d60cb2cf62cb7cdc334d7c15158b3a
Bruno Silva <bruno_silva2101@hotmail.com>
2023-09-11 18:36:22 -0300

    test: view in production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import React from 'react'
 
 export default function Welcome() {
   const router = useRouter()
   router.push('/pt')
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  )
 }
